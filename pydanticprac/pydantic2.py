763ef3471894768884ea5772ce83504ab065f467
Nishant Pandey <pandeynishantsagar@gmail.com>
2026-01-15 12:55:30 +0000

    secondcommit

--- pydanticprac/pydantic2.py
+++ pydanticprac/pydantic2.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel,ValidationError
+from pydantic import BaseModel,ValidationError,Field
 class User(BaseModel):
     uid: int
     username:str
@@ -9,10 +9,24 @@ class User(BaseModel):
     bio: str=""
     full_name: str|None=None
 
+class Item(BaseModel):
+   itemid: int
+   itemname: str
+   itemprice: int 
+   purchased_at: datetime | None=None
+   expired: bool=False
+   ingredients: list[str]=Field(default_factory=list)
+
+
 try:
  user1=User(uid=123,username="Nishant",email="Nishant@gmail.com",age=22)
- user1.bio=123
- print(user1)
+ item1=Item(itemid=1,itemname="cake",itemprice=30,ingredients=['sugar','milk'])
+ print(item1.ingredients)
+ item2=Item(itemid=2,itemname="pastry",itemprice=40,ingredients=['egg','aginomoto'])
+ print(item2.ingredients)
+ user1.bio="123"
+ print(user1.model_dump())
+ print(user1.model_dump_json(indent=2))
  print(user1.username)
 except ValidationError as e:
-   print("the error is:",e) 
+   print("Please Check:",e) 
